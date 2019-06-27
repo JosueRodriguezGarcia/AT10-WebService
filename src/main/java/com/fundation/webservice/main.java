cb05114d73f50ebde4d64c2972e01f20d5fd8f2f
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-06-27 13:22:25 -0400

    Implement REST Controller

--- src/main/java/com/fundation/webservice/main.java
+++ src/main/java/com/fundation/webservice/main.java
@@ -1,4 +1,26 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 package com.fundation.webservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Implements a dummy client for testing the REST controller
+ *
+ * @author Alejandro Sánchez Luizaga
+ * @version 0.1.0
+ */
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
