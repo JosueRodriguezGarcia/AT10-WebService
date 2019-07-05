6a22e558c7c20f60037c96ec70e318bc132d7cf4
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-07-05 13:19:44 -0400

    Implement PdfToHtml criteria & converter

---
+++ src/main/java/com/fundation/webservice/model/PdfToHtml.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+package com.fundation.webservice.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Implements a Pdf To Html converter class.
+ *
+ * @author Alejandro Sanchez
+ * @version 1.0
+ */
+public class PdfToHtml implements IConvert{
+    CriteriaPdfToHtml criteriaPdfToHtml;
+
+    public PdfToHtml (CriteriaPdfToHtml criteriaConvert) {
+        this.criteriaPdfToHtml = criteriaConvert;
+    }
+
+    public void convert() {
+        String s = null;
+
+        try {
+            Process p = Runtime.getRuntime().exec("\\Users\\AlejandroSanchez\\Desktop\\alszla\\_i\\poppler\\bin\\pdftohtml.exe -c -noframes " + criteriaPdfToHtml.getSrcPath() + " " + criteriaPdfToHtml.getDestPath());
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+            System.exit(0);
+        }
+       catch (IOException e) {
+           e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
