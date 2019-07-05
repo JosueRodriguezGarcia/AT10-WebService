7d39b94d26260dcab01df3afd1e1d70f162ffdf2
Limbert Vargas Laura <LimbertVargas@Fundacion-jala.local>
2019-07-05 10:25:05 -0400

    fix observations comments in the code

--- src/main/java/com/fundation/webservice/model/VideoConvert.java
+++ src/main/java/com/fundation/webservice/model/VideoConvert.java
@@ -25,41 +25,35 @@ import java.io.File;
  * @version 1.0
  */
 public class VideoConvert implements IConvert {
-
     private CriteriaVideo criterion;
     public VideoConvert(CriteriaVideo criterion){
-        this.criterion=criterion;
+        this.criterion = criterion;
     }
 
     public void convert() {
         try {
             File source = new File(criterion.getSrcPath());
             File target = new File(criterion.getDestPath());
-
             //Audio Attributes
             AudioAttributes audio = new AudioAttributes();
             audio.setCodec(criterion.getaCodec());
             audio.setBitRate(criterion.getaBit());
             audio.setChannels(criterion.getaChannel());
             audio.setSamplingRate(criterion.getaRate());
-
             //Video Attributes
             VideoAttributes video = new VideoAttributes();
             video.setCodec(criterion.getvCodec());
             video.setTag(criterion.getvTag());
             video.setBitRate(new Integer(criterion.getvBit()));
             video.setFrameRate(new Integer(criterion.getvRate()));
-
             //Encoding attributes
             EncodingAttributes attrs = new EncodingAttributes();
             attrs.setFormat(criterion.getNewFormat());
             attrs.setAudioAttributes(audio);
             attrs.setVideoAttributes(video);
-
             //Encode
             Encoder encoder = new Encoder();
             encoder.encode(new MultimediaObject(source), target, attrs);
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
