60391e9caaebfe36e59c961c788346f597e31896
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-07-08 10:02:29 -0400

    Implement Html To Docx Criteria & Converter

---
+++ src/main/java/com/fundation/webservice/model/CriteriaHtmlToDoc.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+package com.fundation.webservice.model;
+
+/**
+ * Implements a Criteria class (specific to HTML -> DOC conversion) that extends a CriteriaConvert superclass.
+ *
+ * @author Alejandro Sanchez
+ * @version 1.0
+ */
+public class CriteriaHtmlToDoc extends CriteriaConvert {
+}
