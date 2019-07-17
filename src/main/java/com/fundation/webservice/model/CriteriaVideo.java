cbda69ae7c2597e5424e1b83e60364c9350a9b43
Limbert Vargas Laura <LimbertVargas@Fundacion-jala.local>
2019-07-17 14:12:41 -0400

    adding documentation and clean code

--- src/main/java/com/fundation/webservice/model/CriteriaVideo.java
+++ src/main/java/com/fundation/webservice/model/CriteriaVideo.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2019 Jalasoft.
  *
  * This software is the confidential and proprietary information of Jalasoft.
@@ -12,90 +12,186 @@ package com.fundation.webservice.model;
 /**
  * Implements the model class File and the getter and setter´s methods
  *
- * @author Maday Alcala
+ * @author Maday Alcala, Limbert Vargas
  * @version 1.0
  */
 public class CriteriaVideo extends CriteriaConvert{
     private String newFormat;
-    //audio
+
+    /**
+     * Audio variables.
+     */
     private String audioCodec;
     private int audioBit;
     private int audioChannel;
     private int audioRate;
-    //video
+
+    /**
+     * video variables.
+     */
     private String videoCodec;
     private String videoTag;
     private int videoBit;
     private int videoRate;
 
+    /**
+     * Uses for return output format file.
+     *
+     * @return output format file.
+     */
     public String getNewFormat() {
         return newFormat;
     }
 
+    /**
+     * Uses for modified output format file.
+     *
+     * @param newFormat output format file.
+     */
     public void setNewFormat(String newFormat) {
         this.newFormat = newFormat;
     }
 
+    /**
+     * Uses for return audio codec format.
+     *
+     * @return audio codec format.
+     */
     public String getAudioCodec() {
         return audioCodec;
     }
 
+    /**
+     * Uses for modified audio codec format.
+     *
+     * @param audioCodec audio codec format.
+     */
     public void setAudioCodec(String audioCodec) {
         this.audioCodec = audioCodec;
     }
 
+    /**
+     * Uses for return bit rate of the output file.
+     *
+     * @return bit rate of the output file.
+     */
     public int getAudioBit() {
         return audioBit;
     }
 
+    /**
+     * Uses for modified bit rate of the output file.
+     *
+     * @param audioBit bit rate of the output file.
+     */
     public void setAudioBit(int audioBit) {
         this.audioBit = audioBit;
     }
 
+    /**
+     * Uses for return number of channels required in the output file.
+     *
+     * @return number of channels required in the output file.
+     */
     public int getAudioChannel() {
         return audioChannel;
     }
 
+    /**
+     * Uses for modified number of channels required in the output file.
+     *
+     * @param audioChannel number of channels required in the output file.
+     */
     public void setAudioChannel(int audioChannel) {
         this.audioChannel = audioChannel;
     }
 
+    /**
+     * Uses for return audio rate of the output file.
+     *
+     * @return audio rate of the output file.
+     */
     public int getAudioRate() {
         return audioRate;
     }
 
+    /**
+     * Uses for modified audio rate of the output file.
+     *
+     * @param audioRate audio rate of the output file.
+     */
     public void setAudioRate(int audioRate) {
         this.audioRate = audioRate;
     }
 
+    /**
+     * Uses for return video codec format of the output video file.
+     *
+     * @return video codec format of the output video file.
+     */
     public String getVideoCodec() {
         return videoCodec;
     }
 
+    /**
+     * Uses for modified video codec format of the output video file.
+     *
+     * @param videoCodec video codec format of the output video file.
+     */
     public void setVideoCodec(String videoCodec) {
         this.videoCodec = videoCodec;
     }
 
+    /**
+     * Uses for return video tag of the output video file.
+     *
+     * @return video tag of the output video file.
+     */
     public String getVideoTag() {
         return videoTag;
     }
 
+    /**
+     * Uses for modified video tag of the output video file.
+     *
+     * @param videoTag video tag of the output video file.
+     */
     public void setVideoTag(String videoTag) {
         this.videoTag = videoTag;
     }
 
+    /**
+     * Uses for return video bit rate.
+     *
+     * @return video bit rate.
+     */
     public int getVideoBit() {
         return videoBit;
     }
 
+    /**
+     * Uses for modified video bit rate.
+     *
+     * @param videoBit video bit rate.
+     */
     public void setVideoBit(int videoBit) {
         this.videoBit = videoBit;
     }
 
+    /**
+     * Uses for return rate of the output video file.
+     *
+     * @return rate of the output video file.
+     */
     public int getVideoRate() {
         return videoRate;
     }
 
+    /**
+     * Uses for modified rate of the output video file.
+     *
+     * @param videoRate rate of the output video file.
+     */
     public void setVideoRate(int videoRate) {
         this.videoRate = videoRate;
     }
--- src/main/java/com/fundation/webservice/model/VideoResponse.java
+++ src/main/java/com/fundation/webservice/model/VideoResponse.java
@@ -12,7 +12,7 @@ package com.fundation.webservice.model;
 /**
  * Implements a type of response to an /upload request.
  *
- * @author Alejandro Sanchez Luizaga, Maday Alcala Cuba
+ * @author Alejandro Sanchez Luizaga, Maday Alcala Cuba, Limbert Vargas
  * @version 1.1
  */
 public class VideoResponse {
@@ -21,19 +21,39 @@ public class VideoResponse {
     private String fileType;
     private long size;
     private String newFormat;
-    //audio
+    /**
+     * Audio  variables.
+     */
     private String audioCodec;
     private String audioBit;
     private String audioChannel;
     private String audioRate;
-    //video
+    /**
+     * Video variables.
+     */
     private String videoCodec;
     private String videoTag;
     private String videoBit;
     private String videoRate;
-
     private String checksum;
 
+    /**
+     *
+     * @param fileName defines de name os input file.
+     * @param fileDownloadUri defines destination URL direction.
+     * @param fileType defines de extention of the input file.
+     * @param size defines the size of the file.
+     * @param newFormat defines the output format file.
+     * @param audioCodec defines the audio codec format.
+     * @param audioBit defines the bit rate of the output file.
+     * @param audioChannel defines the number of channels required in the output file.
+     * @param audioRate defines the audio rate of the output file.
+     * @param videoCodec defines the video codec format of the output video file.
+     * @param videoTag defines the video tag of the output video file.
+     * @param videoBit defines the video bit rate.
+     * @param videoRate defines the rate of the output video file.
+     * @param checksum defines the checksum of the output or input file.
+     */
     public VideoResponse(String fileName, String fileDownloadUri, String fileType, long size,
                          String newFormat, String audioCodec, String audioBit, String audioChannel, String audioRate,
                          String videoCodec, String videoTag, String videoBit, String videoRate, String checksum) {
@@ -42,10 +62,18 @@ public class VideoResponse {
         this.fileType = fileType;
         this.size = size;
         this.newFormat = newFormat;
+
+        /**
+         * Audio constructors.
+         */
         this.audioCodec = audioCodec;
         this.audioBit = audioBit;
         this.audioChannel = audioChannel;
         this.audioRate = audioRate;
+
+        /**
+         * Audio constructors.
+         */
         this.videoCodec = videoCodec;
         this.videoTag = videoTag;
         this.videoBit = videoBit;
@@ -53,57 +81,127 @@ public class VideoResponse {
         this.checksum = checksum;
     }
 
+    /**
+     * Uses for send the file name of the input file.
+     *
+     * @return file name of the input file
+     */
     public String getFileName() {
         return fileName;
     }
 
+    /**
+     * Uses for destination URL direction.
+     *
+     * @return destination URL direction.
+     */
     public String getFileDownloadUri() {
         return fileDownloadUri;
     }
 
+    /**
+     * Uses for extention of the input file.
+     *
+     * @return extention of the input file.
+     */
     public String getFileType() {
         return fileType;
     }
 
+    /**
+     * Uses for size of the file.
+     *
+     * @return size of the file.
+     */
     public long getSize() {
         return size;
     }
 
+    /**
+     * Uses for output format file.
+     *
+     * @return output format file.
+     */
     public String getNewFormat() {
         return newFormat;
     }
 
+    /**
+     * Uses for audio codec format.
+     *
+     * @return audio codec format.
+     */
     public String getAudioCodec() {
         return audioCodec;
     }
 
+    /**
+     * Uses for bit rate of the output file.
+     *
+     * @return bit rate of the output file.
+     */
     public String getAudioBit() {
         return audioBit;
     }
 
+    /**
+     * Uses for number of channels required in the output file.
+     *
+     * @return number of channels required in the output file.
+     */
     public String getAudioChannel() {
         return audioChannel;
     }
 
+    /**
+     * Uses for audio rate of the output file.
+     *
+     * @return audio rate of the output file.
+     */
     public String getAudioRate() {
         return audioRate;
     }
 
+    /**
+     * Uses for video codec format of the output video file.
+     *
+     * @return video codec format of the output video file.
+     */
     public String getVideoCodec() {
         return videoCodec;
     }
 
+    /**
+     * Uses for video tag of the output video file.
+     *
+     * @return video tag of the output video file.
+     */
     public String getVideoTag() {
         return videoTag;
     }
 
+    /**
+     * Uses for video bit rate.
+     *
+     * @return video bit rate.
+     */
     public String getVideoBit() {
         return videoBit;
     }
 
+    /**
+     * Uses for rate of the output video file.
+     *
+     * @return rate of the output video file.
+     */
     public String getVideoRate() {
         return videoRate;
     }
 
+    /**
+     * Uses for checksum of the output or input file.
+     *
+     * @return checksum of the output or input file.
+     */
     public String getChecksum(){ return checksum; }
 }
