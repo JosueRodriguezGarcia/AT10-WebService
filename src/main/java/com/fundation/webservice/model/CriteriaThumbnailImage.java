86b54ca5448779fd934b737e13f32cba9933033d
Maday Alcala <maday.alcala@fundacion-jala.org>
2019-07-20 11:08:48 -0400

    Adding 'CriteriaThumbnailImage' class

---
+++ src/main/java/com/fundation/webservice/model/CriteriaThumbnailImage.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+package com.fundation.webservice.model;
+
+/**
+ * Implements the criteria for create a Image Thumbnail.
+ *
+ * @author Maday Alcalá Cuba
+ * @version 1.0
+ */
+public class CriteriaThumbnailImage extends CriteriaConvert {
+    private String name;
+    private String time;
+    private String ext;
+
+    //Returns the name of the output file.
+    public String getName() {
+        return name;
+    }
+
+    //Modifies the name of the output file.
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Returns the time of the frame that we search.
+    public String getTime() {
+        return time;
+    }
+
+    //Modifies the time of the frame that we search.
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    //Returns the extension of the output file.
+    public String getExt() {
+        return ext;
+    }
+
+    //Modifies the extension od the output file.
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+}
