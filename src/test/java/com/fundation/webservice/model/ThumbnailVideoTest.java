349552d2bfea08670ea988e00580ce9cedd64246
JosueRodriguezGarcia <Josue.Rodriguez@fundacion-jala.org>
2019-07-24 12:11:37 -0400

    add commnets in document in ThumbnailVideo class

--- src/test/java/com/fundation/webservice/model/ThumbnailVideoTest.java
+++ src/test/java/com/fundation/webservice/model/ThumbnailVideoTest.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 package com.fundation.webservice.model;
 
 import org.junit.Test;
@@ -5,6 +14,13 @@ import org.junit.Test;
 import java.io.File;
 
 import static org.junit.Assert.*;
+
+/**
+ * This class serves as Junit test class for Thumbnail class.
+ *
+ * @author Josue Rodriguez Garcia
+ * @version 1.0
+ */
 
 public class ThumbnailVideoTest {
 
