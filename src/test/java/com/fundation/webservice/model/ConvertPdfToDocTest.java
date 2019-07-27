cf8d600135b19cde11d8b2c98363b69cc358ebda
JosueRodriguezGarcia <Josue.Rodriguez@fundacion-jala.org>
2019-07-27 09:27:50 -0400

    fix space and documentation

--- src/test/java/com/fundation/webservice/model/ConvertPdfToDocTest.java
+++ src/test/java/com/fundation/webservice/model/ConvertPdfToDocTest.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 package com.fundation.webservice.model;
 
 import org.junit.Test;
@@ -6,8 +15,13 @@ import java.io.File;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * This class serves as unit test class for ConvertPdfToDoc class.
+ *
+ * @author Josue Rodriguez Garcia
+ * @version 1.0
+ */
 public class ConvertPdfToDocTest {
-
     @Test
     public void convert_pdf_doc() {
         CriteriaPdfToDoc criteriaPdfToDoc = new CriteriaPdfToDoc();
