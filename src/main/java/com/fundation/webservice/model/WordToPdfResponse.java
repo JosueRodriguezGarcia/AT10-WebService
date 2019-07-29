a638db34ac61834426e430549d0a3fe825be0616
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-07-28 21:57:41 -0400

    correct some fix

--- src/main/java/com/fundation/webservice/model/WordToPdfResponse.java
+++ src/main/java/com/fundation/webservice/model/WordToPdfResponse.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2019 Jalasoft.
- * <p>
+ *
  * This software is the confidential and proprietary information of Jalasoft.
  * ("Confidential Information"). You shall not
  * disclose such Confidential Information and shall use it only in
@@ -12,7 +12,7 @@ package com.fundation.webservice.model;
 /**
  * Implements a type of response to an /upload request.
  *
- * @author Alejandro Sanchez Luizaga, Maday Alcala Cuba, Limbert Vargas, Jesus Menacho
+ * @author Alejandro Sanchez Luizaga, Maday Alcala Cuba, Limbert Vargas, Jesus Menacho.
  * @version 1.1
  */
 
@@ -23,32 +23,62 @@ public class WordToPdfResponse extends Response {
     private long size;
     private String checksum;
 
+    /**
+     * This constructor let setting variables.
+     * @param fileName this is parameter the file name.
+     * @param fileDownloadUri this is the
+     * @param checksum
+     */
     public WordToPdfResponse(String fileName, String fileDownloadUri, String checksum) {
         this.fileName = fileName;
         this.fileDownloadUri = fileDownloadUri;
         this.checksum = checksum;
     }
 
+    /**
+     * This method get a file name.
+     * @return the name file name.
+     */
     public String getFileName() {
         return fileName;
     }
 
+    /**
+     * This method let setting a fileName.
+     * @param fileName return the file name.
+     */
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
 
+    /**
+     * This method let get a file.
+     * @return the file variable.
+     */
     public String getFileDownloadUri() {
         return fileDownloadUri;
     }
 
+    /**
+     * This method let get fileDownloadUri.
+     * @param fileDownloadUri the directions to the file.
+     */
     public void setFileDownloadUri(String fileDownloadUri) {
         this.fileDownloadUri = fileDownloadUri;
     }
 
+    /**
+     * This variable let get a checksum.
+     * @return the checksum variable.
+     */
     public String getChecksum() {
         return checksum;
     }
 
+    /**
+     * This method let setting a variable checksum.
+     * @param checksum this varable the checksum that have a file.
+     */
     public void setChecksum(String checksum) {
         this.checksum = checksum;
     }
