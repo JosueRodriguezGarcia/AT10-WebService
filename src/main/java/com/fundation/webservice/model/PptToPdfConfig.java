a15b894b95ec048d26c50bed5ea328bb39f74441
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-07-29 21:13:41 -0400

    Add pptToPdf conversion to Controller endpoint

---
+++ src/main/java/com/fundation/webservice/model/PptToPdfConfig.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+package com.fundation.webservice.model;
+
+/**
+ * @author Alejandro Sánchez Luizaga
+ * @version 1.0
+ */
+public enum PptToPdfConfig {
+    metadata(""),
+    thumbnail("false");
+    private String value;
+
+    PptToPdfConfig(String value) {
+        this.value = value;
+    }
+
+    /**
+     * This is a getter method that returns the string paired to its corresponding enum element.
+     *
+     * @return the string paired to a enum element
+     */
+    public String getValue() {
+        return value;
+    }
+}
