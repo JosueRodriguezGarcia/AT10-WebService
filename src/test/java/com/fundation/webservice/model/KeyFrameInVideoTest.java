726200bbe4419c95c694bc49e03eee68eea6e365
JosueRodriguezGarcia <Josue.Rodriguez@fundacion-jala.org>
2019-07-30 09:44:16 -0400

    fix keyframe class

--- src/test/java/com/fundation/webservice/model/KeyFrameInVideoTest.java
+++ src/test/java/com/fundation/webservice/model/KeyFrameInVideoTest.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 package com.fundation.webservice.model;
 
 import org.junit.Test;
@@ -6,9 +15,15 @@ import java.io.File;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * This class serves as unit test class for keyframe in Video class.
+ *
+ * @author Josue Rodriguez
+ * @version 1.0
+ */
 public class KeyFrameInVideoTest {
-
     CriteriaKeyFrameVideo criteriaKeyFrameVideo = new CriteriaKeyFrameVideo();
+
     @Test
     public void convert_video_videoWithKeyFrame() {
         criteriaKeyFrameVideo.setSrcPath(Directories.RSRC_DIR.getDir() + "videoTest.ogv");
