4e8afc2bb5187a94b8dcf802f897fa4f3b24be6a
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-08-01 18:12:12 -0400

    Add the test verifyInsertChecksumInDB

---
+++ src/test/java/com/fundation/webservice/database/QueryTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ *
+ */
+
+package com.fundation.webservice.database;
+
+import com.fundation.webservice.common.Util;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Class that allows to carry out the tests to the queries the database.
+ *
+ * @author Jesus Menacho
+ * @version 1.0
+ */
+public class QueryTest {
+
+    Query query = new Query();
+
+    @Before
+    public void settingVariable() {
+        query.deleteByCheckSum("test1");
+        Calendar c = new GregorianCalendar();
+        String date = "31";
+        String moth = "07";
+        String year = "2019";
+        String saveDate = year + "-" + moth + "-" + date;
+        String day_file = Util.getInstance().getConfig().getDay();
+        query.insertChecksum("test1", day_file, saveDate, "//testPath/path/file.mp4");
+    }
+
+    @Test
+    public void insertChecksum_verifyInsertChecksumInDB() {
+        List<String> infContent = new ArrayList<String>();
+        infContent = query.showContent("test1");
+        String actual = "test1";
+        String expected = infContent.get(0);
+        assertEquals(expected, actual);
+    }
+}
