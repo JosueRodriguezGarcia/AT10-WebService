132ec56e554b2ca697a61c570701432ce8a9ae53
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-08-02 06:02:17 -0400

    Add more KeyFrameInVideo test cases

--- src/test/java/com/fundation/webservice/model/KeyFrameInVideoTest.java
+++ src/test/java/com/fundation/webservice/model/KeyFrameInVideoTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import java.io.File;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * This class serves as unit test class for keyframe in Video class.
@@ -37,5 +38,38 @@ public class KeyFrameInVideoTest {
                 + "." + criteriaKeyFrameVideo.getExt());
         boolean actual = output.exists();
         assertTrue(actual);
+    }
+
+    @Test
+    public void convert_video_criteriaWithNonExistentKeyframeTime_videoWithoutKeyFrame() {
+        criteriaKeyFrameVideo.setSrcPath(Directories.RSRC_DIR.getDir() + "videoTest.ogv");
+        criteriaKeyFrameVideo.setDestPath(Directories.RSRC_DIR.getDir());
+        criteriaKeyFrameVideo.setTime("100000");
+        criteriaKeyFrameVideo.setName("keyframeInVideo");
+        criteriaKeyFrameVideo.setExt("ogv");
+        KeyFrameInVideo keyFrameInVideo = new KeyFrameInVideo();
+        keyFrameInVideo.convert(criteriaKeyFrameVideo);
+        File output = new File(Directories.RSRC_DIR.getDir() + criteriaKeyFrameVideo.getName()
+                + "." + criteriaKeyFrameVideo.getExt());
+        boolean actual = output.exists();
+        assertTrue(actual);
+    }
+
+    @Test (expected = Exception.class)
+    public void convert_video_criteriaWithNonExistentVideo_noVideo() {
+        criteriaKeyFrameVideo.setSrcPath(Directories.RSRC_DIR.getDir() + "nonExistentVideo.ogv");
+        criteriaKeyFrameVideo.setDestPath(Directories.RSRC_DIR.getDir());
+        criteriaKeyFrameVideo.setTime("100");
+        criteriaKeyFrameVideo.setName("keyframeInVideo");
+        criteriaKeyFrameVideo.setExt("ogv");
+        KeyFrameInVideo keyFrameInVideo = new KeyFrameInVideo();
+        keyFrameInVideo.convert(criteriaKeyFrameVideo);
+    }
+
+    @Test (expected = Exception.class)
+    public void KeyFrameInVideo_blankCriteria_resultIllegalArgumentException() {
+        CriteriaKeyFrameVideo blankCriteria = new CriteriaKeyFrameVideo();
+        KeyFrameInVideo keyFrameInVideo = new KeyFrameInVideo();
+        keyFrameInVideo.convert(blankCriteria);
     }
 }
