df469d968072cae4bea07a8555709fb31f379ae7
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-08-02 08:01:11 -0400

    Add more ThumbnailVideo test cases

--- src/test/java/com/fundation/webservice/model/ThumbnailVideoTest.java
+++ src/test/java/com/fundation/webservice/model/ThumbnailVideoTest.java
@@ -23,13 +23,35 @@ import static org.junit.Assert.*;
  */
 
 public class ThumbnailVideoTest {
-
     @Test
     public void convert() {
         CriteriaThumbnailVideo criteriaThumbnailVideo = new CriteriaThumbnailVideo();
         criteriaThumbnailVideo.setSrcPath(Directories.RSRC_DIR.getDir() + "videoTest.ogv");
         criteriaThumbnailVideo.setDestPath(Directories.RSRC_DIR.getDir());
         criteriaThumbnailVideo.setTime("30");
+        criteriaThumbnailVideo.setName("thumbnail");
+        criteriaThumbnailVideo.setExt("bmp");
+        ThumbnailVideo thumbnailVideo = new ThumbnailVideo();
+        thumbnailVideo.convert(criteriaThumbnailVideo);
+        File outputFile = new File(criteriaThumbnailVideo.getDestPath() + criteriaThumbnailVideo.getName() + "."
+                + criteriaThumbnailVideo.getExt());
+        Boolean actual =  outputFile.exists();
+        assertTrue(actual);
+    }
+
+    @Test (expected = Exception.class)
+    public void convert_blankCriteria_resultException() {
+        CriteriaThumbnailVideo criteriaThumbnailVideo = new CriteriaThumbnailVideo();
+        ThumbnailVideo thumbnailVideo = new ThumbnailVideo();
+        thumbnailVideo.convert(criteriaThumbnailVideo);
+    }
+
+    @Test
+    public void convert_invalidTimeValue_resultException() {
+        CriteriaThumbnailVideo criteriaThumbnailVideo = new CriteriaThumbnailVideo();
+        criteriaThumbnailVideo.setSrcPath(Directories.RSRC_DIR.getDir() + "videoTest.ogv");
+        criteriaThumbnailVideo.setDestPath(Directories.RSRC_DIR.getDir());
+        criteriaThumbnailVideo.setTime("30000000000000000000000000000000000000000000000");
         criteriaThumbnailVideo.setName("thumbnail");
         criteriaThumbnailVideo.setExt("bmp");
         ThumbnailVideo thumbnailVideo = new ThumbnailVideo();
